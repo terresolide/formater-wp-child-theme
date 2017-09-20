28a7575330ae5503b487e0a864e56d87c07f041e
epointal <elisabeth.pointal@free.fr>
2017-09-20 12:12:46 +0200

    Add  SVG manager, to  include svg schema in post

--- functions.php
+++ functions.php
@@ -1,10 +1,50 @@
 
 <?php
 /**
-** activation theme
+** ForM@Ter child theme of aeris-wordpress-theme
 **/
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+}
+
+// Add svg and xml as a supported upload type to Media Gallery
+add_filter( 'upload_mimes', 'svg_upload_mimes');
+// Manage svg file : filter for svg in media gallery...
+
+// Take over xml and svg type in media gallery
+function svg_upload_mimes($existing_mimes = array())
+{
+	if(!isset($existing_mimes['xml'])){
+		$existing_mimes['xml'] = 'application/xml';
+	}
+	if(!isset($existing_mimes['svg'])){
+		$existing_mimes['svg'] = 'image/svg+xml';
+	}
+	return $existing_mimes;
+}
+
+// Embed  svg shortcode instead of link
+add_filter( 'media_send_to_editor',  'svg_media_send_to_editor' , 20, 3 );
+
+function svg_media_send_to_editor($html, $id, $attachment)
+{
+	
+	if (isset($attachment['url']) && preg_match( "/\.svg$/i", $attachment['url'])) {
+		
+		$filter = '[include-svg src=' . $attachment['url'] .' ]';
+		return apply_filters('svg_override_send_to_editor',  $filter , $html, $id, $attachment);
+	}else{
+		return $html;
+	}
+}
+
+// include content  for svg file
+add_shortcode("include-svg", "include_file_svg");
+
+function include_file_svg( $attrs, $html='' ){
+	$url = $attrs["src"];
+	$content = file_get_contents($url);
+	return $content;
 }
