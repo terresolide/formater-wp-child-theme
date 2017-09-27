f6ee7bd11eb05f7e0e3b3d2ccc95c047957e2b94
epointal <elisabeth.pointal@free.fr>
2017-09-27 12:19:13 +0200

    Add shortcode to test formater-pdf-viewer-vjs

--- functions.php
+++ functions.php
@@ -3,10 +3,20 @@
 /**
 ** ForM@Ter child theme of aeris-wordpress-theme
 **/
+
+
+$_formater_count = 0;
+
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
 
+}
+add_action( 'wp_enqueue_scripts', 'formater_register_script' );
+function formater_register_script(){
+	wp_register_script('pdf_vjs', "http://localhost/poleter/dist/formater-pdf-viewer-vjs_0.1.0.js", Array(), null, true);
 }
 
 // Add svg and xml as a supported upload type to Media Gallery
@@ -40,6 +50,21 @@ function svg_media_send_to_editor($html, $id, $attachment)
 	}
 }
 
+// Embed  pdf shortcode instead of link
+add_filter( 'media_send_to_editor',  'pdf_media_send_to_editor' , 21, 3 );
+
+function pdf_media_send_to_editor($html, $id, $attachment)
+{
+	
+	if (isset($attachment['url']) && preg_match( "/\.pdf$/i", $attachment['url'])) {
+		
+		$filter = '[embed-pdf src=' . $attachment['url'] .' ]';
+		return apply_filters('pdf_override_send_to_editor',  $filter , $html, $id, $attachment);
+	}else{
+		return $html;
+	}
+}
+
 // include content  for svg file
 add_shortcode("include-svg", "include_file_svg");
 
@@ -51,4 +76,19 @@ function include_file_svg( $attrs, $html='' ){
 	}else{
 		return $svg;
 	}
+	
+	
+}
+
+add_shortcode("embed-pdf", "embed_pdf");
+
+function embed_pdf( $attrs, $html='' ){
+	global $_formater_count;
+	if( $_formater_count == 0){
+		wp_enqueue_script('pdf_vjs');
+		$_formater_count++;
+	}
+	$url = $attrs["src"];
+	echo '<formater-pdf-viewer src="' .$url. '"></formater-pdf-viewer>';
+	
 }
