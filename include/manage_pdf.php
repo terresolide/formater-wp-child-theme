1fbef1126cb6eef614838b444b5bacd7e4395eca
epointal <elisabeth.pointal@free.fr>
2017-09-29 10:01:21 +0200

    load script from fawgit

--- include/manage_pdf.php
+++ include/manage_pdf.php
@@ -5,11 +5,15 @@
  */
 
 $_formater_pdf_count = 0;
-
+$_formater_pdf_viewer_version = '0.1.1';
 add_action( 'wp_enqueue_scripts', 'formater_register_script' );
 
 function formater_register_script(){
-	wp_register_script('pdf_vjs', "https://cdn.rawgit.com/epointal/formater-pdf-viewer-vjs/26490fc7/dist/formater-pdf-viewer-vjs_0.1.1.js", Array(), null, true);
+	global $_formater_pdf_viewer_version;
+	$url = "https://cdn.rawgit.com/epointal/formater-pdf-viewer-vjs/".$_formater_pdf_viewer_version;
+	$url .= "/dist/formater-pdf-viewer-vjs_". $_formater_pdf_viewer_version.".js";
+	wp_register_script('pdf_vjs', $url , Array(), null, true);
+	
 }
 
 // Embed  pdf shortcode instead of link
@@ -30,15 +34,18 @@ function pdf_media_send_to_editor($html, $id, $attachment)
 add_shortcode("embed-pdf", "embed_pdf");
 
 function embed_pdf( $attrs, $html='' ){
-	global $_formater_pdf_count;
+	global $_formater_pdf_count, $_formater_pdf_viewer_version;
 	$url = $attrs["src"];
 	
 	if( $_formater_pdf_count == 0){
 		wp_enqueue_script('pdf_vjs');
 		$_formater_pdf_count++;
 	}
+	//$url_js = "https://cdn.rawgit.com/epointal/formater-pdf-viewer-vjs/".$_formater_pdf_viewer_version;
+	//$url_js .= "/dist/formater-pdf_viewer-vjs_". $_formater_pdf_viewer_version.".js";
 	
 	return  '<formater-pdf-viewer src="' .$url. '"></formater-pdf-viewer>';
-// <script type="text/javascript" component="epointal/formater-pdf-viewer-vjs@latest" src="https://rawgit.com/aeris-data/aeris-component-loader/master/aerisComponentLoader.js" ></script>';
+   //  <script type="text/javascript" src="'.$url_js.'" ></script>';
+// script type="text/javascript" component="epointal/formater-pdf-viewer-vjs@latest" src="https://rawgit.com/aeris-data/aeris-component-loader/master/aerisComponentLoader.js" ></script>';
 	
 }	
