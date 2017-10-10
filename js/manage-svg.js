850b3b1faddaf3acb9ce0a7e4e325537c35d28e8
elisabethP <elisabeth.pointal@free.fr>
2017-10-10 21:22:30 +0200

    transition when enlarge svg + newtag

--- js/manage-svg.js
+++ js/manage-svg.js
@@ -4,10 +4,13 @@
 function formater_switch_svg( node, value ){
     if( value ){
         var classname = "fm-right";
+        var float = "right";
     }else{
-        var classname = "fm-left"
+        var classname = "fm-left";
+        var float = "left";
     }
     console.log(node.parentNode);
+    node.parentNode.style.float = float;
     if(node.parentNode.className.indexOf( classname )>=0){
         node.parentNode.className = node.parentNode.className.replace(classname, "");
     }else{
